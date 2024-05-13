a139edbc4696a04c0ac61b42725fe5b5ce545c1d
egemoroglu <egemoroglu@hotmail.com>
2024-05-13 11:37:02 +0300

    Application Structure

--- ClientSide/src/App.tsx
+++ ClientSide/src/App.tsx
@@ -8,7 +8,6 @@ function App() {
 
 
   return (
-    <>
     <div className="App">
       <div className='navbar'>
         <h2>Todo App</h2>
@@ -21,8 +20,6 @@ function App() {
         </Routes>
       </Router>
     </div>
-      
-    </>
   )
 }
 
