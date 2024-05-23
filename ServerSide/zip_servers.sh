0620d38b8f0ef557c3a66612e2d670c8dcfd04d3
egemoroglu <egemoroglu@hotmail.com>
2024-05-23 11:33:45 +0300

    Github Actions Terraform

--- ServerSide/zip_servers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-# Zip TodoServer folder
-zip -j todo-server.zip TodoServer/build/server.js
-
-# Zip UserServer folder
-zip -j user-server.zip UserServer/build/server.js
